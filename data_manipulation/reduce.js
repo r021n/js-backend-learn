fa7da05b601892ca7157f7244d34008e9c7306c6
r021n <rozinabdul25@gmail.com>
2025-02-09 15:19:07 +0700

    use reduce for count shop total

--- data_manipulation/reduce.js
+++ data_manipulation/reduce.js
@@ -7,20 +7,33 @@
 
 // =====> contoh 1 (penjumlahan semua elemen array)
 
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-const jumlahAngka = numbers.reduce(function (acc, curr) {
-    return acc + curr;
-}, 0)
+// const jumlahAngka = numbers.reduce(function (acc, curr) {
+//     return acc + curr;
+// }, 0)
 
-// console.log(jumlahAngka);
+// // console.log(jumlahAngka);
 
-// =====> contoh 2 (mencari nilai terbesar)
+// // =====> contoh 2 (mencari nilai terbesar)
 
-const numbers2 = [10, 3, -2, 1, 22, 11, -22];
+// const numbers2 = [10, 3, -2, 1, 22, 11, -22];
 
-const maxNumber = numbers2.reduce((acc, curr) => {
-    return Math.max(acc, curr);
-}, numbers2[0]);
+// const maxNumber = numbers2.reduce((acc, curr) => {
+//     return Math.max(acc, curr);
+// }, numbers2[0]);
 
-console.log(maxNumber);
+// console.log(maxNumber);
+
+// contoh menghitung total belanja
+const keranjangBelanja = [
+    {nama: "Laptop", harga: 12000000, jumlah: 1},
+    {nama: "Mouse", harga: 100000, jumlah: 2},
+    {nama: "Keyboard", harga: 200000, jumlah: 1},
+]
+
+const totalBelanja = keranjangBelanja.reduce(function(acc, curr) {
+    return acc + (curr.harga * curr.jumlah);
+}, 0);
+
+console.log(totalBelanja);
